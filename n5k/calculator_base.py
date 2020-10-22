765c71f4e0d323270b4276aa6f8abc8088503094
c-d-leonard <c.danielle.leonard@gmail.com>
2020-10-22 13:26:06 +0100

    remove IA from base class

--- n5k/calculator_base.py
+++ n5k/calculator_base.py
@@ -37,10 +37,7 @@ class N5KCalculatorBase(object):
                         1.607983, 1.689579, 1.772899,
                         1.857700, 1.943754, 2.030887,
                         2.118943])
-        # Amplitude of NLA model (accounting for f_red already)
-        A_IA = np.full(5, 0.15)
-        return {'b_g': b_g,
-                'A_IA': A_IA}
+        return {'b_g': b_g}
 
     def get_tracer_dndzs(self):
         dNdz_file = np.load('input/dNdzs.npz')
