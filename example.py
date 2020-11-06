442fa6373d68f83b795cbd53cd73217c666f1d12
c-d-leonard <c.danielle.leonard@gmail.com>
2020-11-06 15:09:07 +0000

    add comment to example

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import n5k
 
+# The evaluation of challenge entries will also include accuracy and integratbility - this script is just to show an example of timing an entry.
 
 def time_run(cls, config):
     c = cls(config)
