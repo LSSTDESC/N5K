edcd316cb58c2a2ddf165fee7e5e1ee2393985e6
Tilman Troester <tilman.troester@gmail.com>
2021-06-09 12:51:34 +0100

    Updates for ell-parallelisation.

--- n5k/calculator_levin.py
+++ n5k/calculator_levin.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pyccl as ccl
 from .calculator_base import N5KCalculatorBase
 
 import levinpower
@@ -9,8 +8,6 @@ class N5KCalculatorLevin(N5KCalculatorBase):
 
     def setup(self):
         # Initialize cosmology
-        par = self.get_cosmological_parameters()
-
         pk = self.get_pk()
         kernels = self.get_tracer_kernels()
         background = self.get_background()
@@ -18,19 +15,24 @@ class N5KCalculatorLevin(N5KCalculatorBase):
         number_count = kernels["kernels_cl"].shape[0]
 
         precompute_splines = self.config.get('precompute_splines', False)
+        ell = self.get_ells().astype(int)
         self.levin_calculator = levinpower.LevinPower(
                           precompute_splines,
+                          ell,
                           number_count, 
-                          background["z"], background["chi"], 
-                          kernels["chi_cl"], np.concatenate((kernels["kernels_cl"].T, kernels["kernels_sh"].T), axis=1), 
-                          pk["k"], pk["z"], pk["pk_lin"].flatten(), pk["pk_nl"].flatten())
-
+                          background["z"], background["chi"],
+                          kernels["chi_cl"],
+                          np.concatenate((kernels["kernels_cl"].T,
+                                          kernels["kernels_sh"].T), axis=1),
+                          pk["k"], pk["z"],
+                          pk["pk_lin"].flatten(),
+                          pk["pk_nl"].flatten())
 
     def run(self):
         # Compute power spectra
-        ell = self.get_ells().astype(int)
-
-        self.cls_gg, self.cls_gs, self.cls_ss = self.levin_calculator.compute_C_ells(ell)
+        parallelize_ell = self.config.get('parallelize_ell', False)
+        self.cls_gg, self.cls_gs, self.cls_ss = \
+            self.levin_calculator.compute_C_ells(parallelize_ell)
 
         self.cls_gg = np.array(self.cls_gg)
         self.cls_gs = np.array(self.cls_gs)
