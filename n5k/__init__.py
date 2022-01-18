1b6387614f18d3cc9806fdbeb804318f8bc46c6d
David Alonso <dam.phys@gmail.com>
2022-01-18 10:26:55 +0000

    merged matter

--- n5k/__init__.py
+++ n5k/__init__.py
@@ -3,4 +3,5 @@ from .calculator_ccl import N5KCalculatorCCL
 from .calculator_tester import N5KCalculatorTester
 from .calculator_ccl_nl import N5KCalculatorCCLNonLimber
 from .calculator_fkem import N5KCalculatorFKEM
+from .calculator_matter import N5KCalculatorMATTER
 from .utils import n5k_calculator_from_name
