4b4a019275b3b79f9639a85923792b36ede4d8c9
David Alonso <dam.phys@gmail.com>
2022-01-18 10:35:00 +0000

    merged levin

--- n5k/__init__.py
+++ n5k/__init__.py
@@ -4,4 +4,5 @@ from .calculator_tester import N5KCalculatorTester
 from .calculator_ccl_nl import N5KCalculatorCCLNonLimber
 from .calculator_fkem import N5KCalculatorFKEM
 from .calculator_matter import N5KCalculatorMATTER
+from .calculator_levin import N5KCalculatorLEVIN
 from .utils import n5k_calculator_from_name
