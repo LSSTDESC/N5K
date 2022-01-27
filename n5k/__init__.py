8077c6d080fc133e8ece248c121c4ad43e6b7693
schoeneberg <schoeneberg@physik.rwth-aachen.de>
2022-01-27 17:39:47 +0100

    Changes acording to damonge

--- n5k/__init__.py
+++ n5k/__init__.py
@@ -1,6 +1,5 @@
 from .calculator_base import N5KCalculatorBase
 from .calculator_ccl import N5KCalculatorCCL
-from .calculator_matter import N5KCalculatorMATTER
 from .calculator_tester import N5KCalculatorTester
 from .calculator_ccl_nl import N5KCalculatorCCLNonLimber
 from .calculator_fkem import N5KCalculatorFKEM
