370197f0e3baf403133ed09a2a0754d93231c9c0
Danielle Leonard <c.danielle.leonard@gmail.com>
2022-02-17 02:57:53 -0800

    tidy up merging in updates

--- n5k/__init__.py
+++ n5k/__init__.py
@@ -3,4 +3,6 @@ from .calculator_ccl import N5KCalculatorCCL
 from .calculator_tester import N5KCalculatorTester
 from .calculator_ccl_nl import N5KCalculatorCCLNonLimber
 from .calculator_fang import N5KCalculatorFKEM
+from .calculator_levin import N5KCalculatorLevin
+from .calculator_matter import N5KCalculatorMATTER
 from .utils import n5k_calculator_from_name
