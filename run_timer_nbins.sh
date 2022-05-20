2ab37da7efc43603c770a204f69245fd02752ce7
Danielle Leonard <c.danielle.leonard@gmail.com>
2022-05-20 06:59:09 -0700

    infrastructure to run benchmark tests on a subset of bins

--- run_timer_nbins.sh
+++ run_timer_nbins.sh
@@ -2,19 +2,21 @@
 
 mkdir -p outputs
 
-#for i in 10 9 8 7 6 5 4 3 2 1
-#do
-#    echo ${i}
-#    echo FKEM
-#    python timer_nbins.py confs/config_fkem.yml FKEM ${i} 5 >> outputs/times_nb_sh5_FKEM.txt
-#    python timer_nbins.py confs/config_fkem.yml FKEM ${i} 1 >> outputs/times_nb_sh1_FKEM.txt
-#    echo MATTER
-#    python timer_nbins.py confs/config_matter.yml MATTER ${i} 5 >> outputs/times_nb_sh5_MATTER.txt
-#    python timer_nbins.py confs/config_matter.yml MATTER ${i} 1 >> outputs/times_nb_sh1_MATTER.txt
-#    echo Levin
-#    python timer_nbins.py confs/config_levin.yml Levin ${i} 5 >> outputs/times_nb_sh5_Levin.txt
-#    python timer_nbins.py confs/config_levin.yml Levin ${i} 1 >> outputs/times_nb_sh1_Levin.txt
-#done
+for i in 10 9 8 7 6 5 4 3 2 1
+do
+    echo ${i}
+    echo FKEM
+    python timer_nbins.py confs/config_fkem.yml FKEM ${i} 5 >> outputs/times_nb_sh5_FKEM.txt
+    python timer_nbins.py confs/config_fkem.yml FKEM ${i} 1 >> outputs/times_nb_sh1_FKEM.txt
+    exit 1
+    
+    echo MATTER
+    python timer_nbins.py confs/config_matter.yml MATTER ${i} 5 >> outputs/times_nb_sh5_MATTER.txt
+    python timer_nbins.py confs/config_matter.yml MATTER ${i} 1 >> outputs/times_nb_sh1_MATTER.txt
+    echo Levin
+    python timer_nbins.py confs/config_levin.yml Levin ${i} 5 >> outputs/times_nb_sh5_Levin.txt
+    python timer_nbins.py confs/config_levin.yml Levin ${i} 1 >> outputs/times_nb_sh1_Levin.txt
+done
 
 for i in 5 4 3 2 1
 do
